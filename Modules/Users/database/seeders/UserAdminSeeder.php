8198fe30aba548fb358a0c1b1d093ffd2419d6f7
Tu Ngoc Van <tungocvan@gmail.com>
2026-08-03 13:18:47 +0700

    03082026-vnh-2

--- Modules/Users/database/seeders/UserAdminSeeder.php
+++ Modules/Users/database/seeders/UserAdminSeeder.php
@@ -6,22 +6,28 @@ namespace Modules\Users\database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class UserAdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $userAdmin = User::factory()->create([
-            'name' => 'Từ Ngọc Vân',
-            'email' => 'tungocvan@gmail.com',
-            'password' => bcrypt('123456'),
-        ]);
-        $role = Role::findByName('Super Admin');
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $userAdmin = User::query()->updateOrCreate(
+            ['email' => 'tungocvan@gmail.com'],
+            [
+                'name' => 'Từ Ngọc Vân',
+                'password' => bcrypt('123456'),
+                'account_type' => 'system',
+                'is_active' => true,
+            ]
+        );
+
+        $role = Role::findByName('Super Admin', 'admin');
+
         $userAdmin->assignRole($role);
 
-
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
     }
 }
